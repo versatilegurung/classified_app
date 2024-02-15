2c1fa704efdfe78455376a3dd3fad532293f3495
versatilegurung <80646494+versatilegurung@users.noreply.github.com>
2024-02-15 07:58:41 +0000

    errorPageModified

--- resources/views/errors/504.blade.php
+++ resources/views/errors/504.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>504 Gateway Timeout</title>
 
-    @vite(['resources/css/app.css', 'resources/css/custom-styles.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/css/custom.css', 'resources/js/app.js'])
 
 </head>
 
