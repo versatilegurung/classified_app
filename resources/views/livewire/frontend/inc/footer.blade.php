ce0b2630ab61a651a6f5bbff7d32f6a14ab86c97
versatilegurung <subash85@gmail.com>
2024-02-15 16:57:11 +0000

    footer-updated

--- resources/views/livewire/frontend/inc/footer.blade.php
+++ resources/views/livewire/frontend/inc/footer.blade.php
@@ -1,60 +1,35 @@
 <div>
     <!-- Footer -->
     <footer class="bg-gray-900 text-white py-24">
-        <div class="container mx-auto flex flex-wrap">
+        <div class="container mx-auto flex flex-wrap items-center justify-between">
             <!-- Website Description -->
-            <div class="w-full md:w-1/4 lg:w-3/8 px-4 mb-8">
+            <div class="w-full md:w-2/4 px-4 mb-8">
                 <h2 class="text-2xl font-bold mb-4">About Us</h2>
                 <p class="text-sm font-thin leading-tight text-justify ">Welcome to Chitwan Buy & Sell, your go-to destination for hassle-free and cost-free classified ads! We take pride in providing a user-friendly platform that connects buyers and sellers from all walks of life without any financial barriers. At Chitwan Buy & Sell, we believe in fostering a community-driven marketplace where you can easily buy, sell, or trade a diverse range of products and services.</p>
             </div>
 
             <!-- Navigation Menu -->
-            <div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
-                <h2 class="text-2xl font-bold mb-4">Website</h2>
+            <div class="w-full md:w-2/4 px-4 mb-8">
+                <h2 class="text-md font-bold mb-4 text-center">Website Navigation</h2>
                 <nav>
-                    <ul class="text-sm">
-                        <li><a href="#" class="text-gray-400 hover:text-white">Home</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Post Ad</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">Lastest Ads</a></li>
+                    <ul class="text-sm flex gap-4 justify-center">
+                        <li><a href="{{route('home')}}" class="text-gray-400 hover:text-white">Home</a></li>
+                        <li><a href="{{route('post.ad')}}" class="text-gray-400 hover:text-white">Post Ad</a></li>
                         <li><a href="#" class="text-gray-400 hover:text-white">Our Services</a></li>
                         <li><a href="#" class="text-gray-400 hover:text-white">Disclaimer</a></li>
                         <li><a href="#" class="text-gray-400 hover:text-white">Contact</a></li>
+                        <li><a href="{{route('login')}}" class="text-gray-400 hover:text-white">Login</a></li>
+                        <li><a href="{{route('register')}}" class="text-gray-400 hover:text-white">Register</a></li>
+
                     </ul>
                 </nav>
             </div>
 
             <!-- Account Links -->
-            <div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
-                <h2 class="text-2xl font-bold mb-4">Account</h2>
-                <nav>
-                    <ul class="text-sm">
-                        <li><a href="#" class="text-gray-400 hover:text-white">My Account</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">My Ads</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">My Wishlist</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">My Alerts</a></li>
-                        <li><a href="#" class="text-gray-400 hover:text-white">My Profile</a></li>
-                    </ul>
-                </nav>
-
+         
 
             <!-- Social Share Links -->
-            <div class="w-full md:w-1/2 lg:w-1/3 px-4 mb-8">
-                <h2 class="text-2xl font-bold mb-4">Connect With Us</h2>
-                <div class="flex space-x-4">
-                    <a href="#" class="text-gray-400 hover:text-white">
-                        <i class="fab fa-facebook"></i>
-                    </a>
-                    <a href="#" class="text-gray-400 hover:text-white">
-                        <i class="fab fa-twitter"></i>
-                    </a>
-                    <a href="#" class="text-gray-400 hover:text-white">
-                        <i class="fab fa-instagram"></i>
-                    </a>
-                    <a href="#" class="text-gray-400 hover:text-white">
-                        <i class="fab fa-linkedin"></i>
-                    </a>
-                </div>
-            </div>
+            
         </div>
         <div></div>
     </footer>
