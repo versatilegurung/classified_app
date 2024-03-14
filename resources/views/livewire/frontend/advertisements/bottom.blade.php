b39d3567a1e85af65e4ad85773ceb7b24f432dad
versatilegurung <subash85@gmail.com>
2024-03-14 10:41:50 +0000

    editAds

--- resources/views/livewire/frontend/advertisements/bottom.blade.php
+++ resources/views/livewire/frontend/advertisements/bottom.blade.php
@@ -1,20 +1,18 @@
 {{-- sponsored ads --}}
-<div class="p-5">
-       
-    <?php
+<?php
     $location = 'bottom';
-
-    // Example code to display ads in a Blade view
     $ads = \App\Models\Advertisement::where('position', $location)->whereDate('start_date', '<=', now())->whereDate('end_date', '>=', now())->limit(1)->get();
     ?>
-
-    @foreach ($ads as $item)
-        <div class="container mx-auto max-w-[900px] px-5">
-            <a href="{{ $item->url }}" target="_blank">
-                <img src="{{ asset( $item->image) }}" alt="{{ $item->title }}" class="w-full h-auto">
-            </a>
-        </div>
-        
-    @endforeach        
-</div>
+@if ($ads->count() > 0)
+    <div class="p-5">
+        @foreach ($ads as $item)
+            <div class="container mx-auto max-w-[900px] px-5">
+                <a href="{{ $item->url }}" target="_blank">
+                    <img src="{{ asset( $item->image) }}" alt="{{ $item->title }}" class="w-full h-auto">
+                </a>
+            </div>
+        @endforeach
+    </div>
+    
+@endif
 {{-- sponsored ads --}}
--- resources/views/livewire/frontend/advertisements/top.blade.php
+++ resources/views/livewire/frontend/advertisements/top.blade.php
@@ -1,19 +1,17 @@
-<div class="mt-16 pt-5 md:pt-10">
-    <?php
-    $location = 'top';
+<?php
+$location = 'top';
+// Example code to display ads in a Blade view
+$ads = \App\Models\Advertisement::where('position', $location)->whereDate('start_date', '<=', now())->whereDate('end_date', '>=', now())->limit(1)->get();
+?>
 
-    // Example code to display ads in a Blade view
-    $ads = \App\Models\Advertisement::where('position', $location)->whereDate('start_date', '<=', now())->whereDate('end_date', '>=', now())->limit(1)->get();
-    ?>
-
-    @foreach ($ads as $item)
-        <div class="container mx-auto max-w-[800px] px-5">
-            <a href="{{ $item->url }}" target="_blank">
-                <img src="{{ asset( $item->image) }}" alt="{{ $item->title }}" class="w-full h-auto">
-            </a>
-        </div>
-        
-    @endforeach        
-  
-    
-</div>
+@if ($ads->count() > 0)
+    <div class="p-5">
+        @foreach ($ads as $item)
+            <div class="container mx-auto max-w-[900px] px-5">
+                <a href="{{ $item->url }}" target="_blank">
+                    <img src="{{ asset( $item->image) }}" alt="{{ $item->title }}" class="w-full h-auto">
+                </a>
+            </div>
+        @endforeach
+    </div>    
+@endif
